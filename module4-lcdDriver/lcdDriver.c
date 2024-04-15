75735ee10eb9bbb766688a86ff62238d06ceb8fb
GauravBh-B66 <g.bhattarai1202@gmail.com>
2024-04-15 16:16:52 +0545

    LCD Driver Completion.

--- module4-lcdDriver/lcdDriver.c
+++ module4-lcdDriver/lcdDriver.c
@@ -11,6 +11,7 @@
 #include <linux/errno.h>
 #include <linux/cdev.h>
 #include <linux/gpio.h>
+#include <linux/delay.h>
 
 #define CLASS_NAME          "class_LCD"
 #define DEVICE_NAME         "device_LCD"
@@ -26,35 +27,7 @@ static int      times = 0;
 static struct class*    pClassLCD;
 static struct device*   pDeviceLCD;
 static struct cdev      cdevLCD;
-
-
-static int deviceFileOpen(struct inode* pInode, struct file* pFile){
-    printk(KERN_INFO "GPIO device file opened. Count = %d.\n", ++times);
-    return 0;
-}
-
-static int deviceFileRelease(struct inode* pInode, struct file* pFile){
-    printk(KERN_INFO "GPIO device file closed/released.");
-    return 0;
-}
-
-static ssize_t deviceFileWrite(struct file* pFile, const char* uBuffer, size_t requestedLength, loff_t* pOffset){
-}
-
-static ssize_t deviceFileRead(struct file* pFile, char* uBuffer, size_t requestedLength, loff_t* pOffset){
-}
-
-static struct file_operations fops ={
-    .owner = THIS_MODULE,
-    .open = deviceFileOpen,
-    .release = deviceFileRelease,
-    .write = deviceFileWrite,
-    .read = deviceFileRead,
-};
-
-#define ENABLE_PIN  controlGpios[0]
-#define REG_SEL_PIN controlGpios[1]
-
+static uint8_t lcdBuffer[17];   //16 characters + '\0'
 
 uint8_t signalGpio[]={
     3,      //  Enable pin
@@ -66,8 +39,89 @@ uint8_t signalGpio[]={
 	10,     //  Data Pin 4
 	9,      //  Data Pin 5
 	11,     //  Data Pin 6
-	5,      //  Data Pin 7 
+	5      //  Data Pin 7 
 };
+
+
+#define ENABLE_PIN  signalGpio[0]
+
+// REG_SEL_PIN = 0: Command mode
+// REG_SEL_PIN = 1: Data mode
+#define REG_SEL_PIN signalGpio[1]
+
+#define CLEAR_SCREEN    0x01
+#define DISPLAY_ON      0x0F        //Turns on the display; blinking cursor.
+
+/*Bit mode configuration
+    0|0|1|DL|N|F|-|-
+        DL: Data length         [0: 4-bit; 1: 8-bit]
+        N:  No of data lines    [0: 1-line; 1: 2-line]
+        F:  Character Font      [0: 5*8 dots; 1: 5*10 dots]
+*/
+#define MODE_8_BIT      0X30    //8-bit; single line
+#define MODE_4_BIT      0x20    //4-bit; single line
+
+
+void enableHigh(void){
+    gpio_set_value(ENABLE_PIN, 1);
+    msleep(5);
+    gpio_set_value(ENABLE_PIN, 0);
+}
+
+void sendCommand(uint8_t data){
+    gpio_set_value(REG_SEL_PIN, 0);
+    for (int i = 0; i <=7; i ++){
+        gpio_set_value (signalGpio[i+2], (data & (1 << i)) >> i);
+    }
+    enableHigh();
+}
+
+void sendData(uint8_t data){
+    gpio_set_value(REG_SEL_PIN, 1);
+    for(int i=0; i<=7; i++){
+        gpio_set_value (signalGpio[i+2], (data & (1 << i)) >> i);
+    }
+    enableHigh();
+}
+
+void setDisplay(void){
+    sendCommand (MODE_8_BIT);
+    sendCommand (DISPLAY_ON);
+    sendCommand (CLEAR_SCREEN);
+}
+
+static int openDevice(struct inode* pInode, struct file* pFile){
+    printk(KERN_INFO "LCD device file opened. Count = %d.\n", ++times);
+    return 0;
+}
+
+static int releaseDevice(struct inode* pInode, struct file* pFile){
+    printk(KERN_INFO "LCD device file closed/released.");
+    return 0;
+}
+
+static ssize_t writeToDevice(struct file* pFile, const char* uBuffer, size_t requestedLength, loff_t* pOffset){
+    int nError, nCopy;
+
+    nCopy = min(requestedLength, sizeof(lcdBuffer));
+    nError = copy_from_user(lcdBuffer, uBuffer, nCopy);
+
+    sendCommand(CLEAR_SCREEN);
+    for (int i=0; i < nCopy; i++){
+        sendData(lcdBuffer[i]);
+    }
+
+    return (nCopy-nError);
+}
+
+
+static struct file_operations fops ={
+    .owner = THIS_MODULE,
+    .open = openDevice,
+    .release = releaseDevice,
+    .write = writeToDevice,
+};
+
 
 static int __init initFunction(void){
     //Out of 12 signal pins of LCD, Contrast Control and READ\WRITE are pins are not supported in this program.
@@ -92,7 +146,7 @@ static int __init initFunction(void){
     printk (KERN_INFO "Initializing the LCD device driver module.\n");
    
     //Allocate device number
-    if ((alloc_chrdev_region(&deviceNumber, 12, 1, DEVICE_FILE_NAME)) < 0 ){
+    if ((alloc_chrdev_region(&deviceNumber, 12, 1, DEVICE_NAME)) < 0 ){
         printk("ERROR: Device number allocation failed.\n");
 		return -1;
     }
@@ -122,7 +176,7 @@ static int __init initFunction(void){
     }
     printk (KERN_INFO "Success: Device file registration.\n");
 
-
+    //Request and set GPIOs to output mode.
     for (count = 0; count < nPins; count++){    
         if(gpio_request(signalGpio[count], pinNames[count]) < 0){
             printk("ERROR: Allocation of %s.\n", pinNames[count]);
@@ -159,16 +213,19 @@ static int __init initFunction(void){
 }
 
 static void __exit exitFunction(void){
-    gpio_set_value(OUT_GPIO, 0);
-    gpio_free(IN_GPIO);
-    gpio_free(OUT_GPIO);
+
+    sendCommand(CLEAR_SCREEN);
+    for (int i=0; i<10; i++){
+        gpio_set_value(signalGpio[i], 0);
+        gpio_free(signalGpio[i]);
+    }
 
     cdev_del(&cdevLCD);
     device_destroy(pClassLCD, deviceNumber);
     class_destroy(pClassLCD);
     unregister_chrdev_region(deviceNumber, 1);
 
-    printk(KERN_INFO"GPIO driver exited.\n");
+    printk(KERN_INFO"LCD driver exited.\n");
 
 }
 
